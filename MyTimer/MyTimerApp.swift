b075474dc272184674beb188ec426600d06cc901
JunichiroM <sbxn4mxt@yahoo.co.jp>
2024-09-20 22:40:28 +0900

    Initial Commit

---
+++ MyTimer/MyTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTimerApp.swift
+//  MyTimer
+//
+//  Created by apple on 2024/09/20.
+//
+
+import SwiftUI
+
+@main
+struct MyTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
