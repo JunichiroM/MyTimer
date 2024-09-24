1893ceacadebe3375175456da64bf521cd95c6e8
JunichiroM <sbxn4mxt@yahoo.co.jp>
2024-09-24 17:29:12 +0900

    i have done up to page 278

--- MyTimer/ContentView.swift
+++ MyTimer/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            ZStack{
+                Image("backgroundTimer")
+                    .resizable()
+                    .ignoresSafeArea()
+                    .scaledToFill()
+                
+                VStack(spacing:30.0){
+                    Text("残り30秒")
+                        .font(.largeTitle)
+                    
+                    HStack{
+                        Button{
+                            
+                        }label: {
+                            Text("スタート")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width:140, height: 140)
+                                .background(Color("startColor"))
+                                .clipShape(Circle())
+                        }
+                        
+                        Button{
+                            
+                        }label: {
+                            Text("ストップ")
+                                .font(.title)
+                                .foregroundColor(Color.white)
+                                .frame(width: 140, height: 140)
+                                .background(Color("stopColor"))
+                                .clipShape(Circle())
+                        }
+                    }
+                }
+            }
+            //ナビゲーションバーの右にボタンを追加
+            .toolbar{
+                //ナビゲーションバーの右にボタンを追加
+                ToolbarItem(placement: .navigationBarTrailing){
+                    //ナビゲーション遷移
+                    NavigationLink{
+                        SettingView()
+                        
+                    }label:{
+                        //テキストを表示
+                        Text("秒数設定")
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
